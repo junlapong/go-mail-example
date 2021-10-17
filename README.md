619317e92c7839dbc6be81c7bb27e6a7615eaa49
Junlapong L <junlapong@gmail.com>
2021-10-17 22:39:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# go-mail-example
+
+## smtp for dev
+
+```
+go install github.com/junlapong/MailHog@latest
+
+MailHog
+```
+
+SMTP Port: 1025
+HTTP URL: http://localhost:8025/
+
---
+++ go_smtp_template/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+  "bytes"
+  "fmt"
+  "net/smtp"
+  "text/template"
+)
+
+func main() {
+
+  // Sender data.
+  from := "from@mail.com"
+  password := "P@ssw0rd"
+
+  // Receiver email address.
+  to := []string{
+    "junlapong@gmail.com",
+  }
+
+  // smtp server configuration.
+  //smtpHost := "smtp.gmail.com"
+  //smtpPort := "587"
+
+  smtpHost := "localhost"
+  smtpPort := "1025"
+
+  // Authentication.
+  auth := smtp.PlainAuth("", from, password, smtpHost)
+
+  t, _ := template.ParseFiles("template.html")
+
+  var body bytes.Buffer
+
+  mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+  body.Write([]byte(fmt.Sprintf("Subject: This is a test subject \n%s\n\n", mimeHeaders)))
+
+  t.Execute(&body, struct {
+    Name    string
+    Message string
+  }{
+    Name:    "Puneet Singh",
+    Message: "This is a test message in a HTML template",
+  })
+
+  // Sending email.
+  err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, body.Bytes())
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println("Email Sent!")
+}
